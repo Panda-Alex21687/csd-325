305e451668b736d88c033d9360e7274b84476b6d
Panda-Alex21687 <Alex21687@gmail.com>
2025-11-06 15:05:44 -0600

    Pushing 11/6/25

--- module-4/sitka_AMB.py
+++ module-4/sitka_AMB.py
@@ -11,7 +11,7 @@
 import csv
 import sys
 from datetime import datetime
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 # ---------------------------------------------------------
 # Function to read CSV data and return date, highs, lows
